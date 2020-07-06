f747d8a3f8296493d83db44201e273775b77a34e
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-07-06 10:33:33 +0700

    Cek lokasi dan toast

--- app/src/main/java/com/example/attendanceapps/MainActivity.kt
+++ app/src/main/java/com/example/attendanceapps/MainActivity.kt
@@ -2,12 +2,15 @@ package com.example.attendanceapps
 
 import android.Manifest
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.provider.Settings
 import android.view.View
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -25,7 +28,35 @@ class MainActivity : AppCompatActivity() {
         onClick()
     }
 
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == ID_LOCATION_PERMISSION){
+            if (grantResults[0]  == PackageManager.PERMISSION_GRANTED ||
+                    grantResults[1] == PackageManager.PERMISSION_GRANTED){
+                Toast.makeText(this, "Berhasil di izinkan", Toast.LENGTH_SHORT).show()
+
+                if (!isLocationEnabled()){
+                    Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+                }
+            }else{
+                Toast.makeText(this, "Gagal di izinkan", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
     private fun checkPermissionLocation() {
+        if (checkPermission()){
+            if (!isLocationEnabled()){
+                Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
+            }
+        }else{
+            requestPermission()
+        }
 
     }
 
