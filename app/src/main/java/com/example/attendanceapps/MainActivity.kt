1df2a439bc6a1204cf4cbd83e42c715c20c9c0c4
Safran Ilmi <49782760+safranilmi@users.noreply.github.com>
2020-07-07 09:45:54 +0700

    Geocode

--- app/src/main/java/com/example/attendanceapps/MainActivity.kt
+++ app/src/main/java/com/example/attendanceapps/MainActivity.kt
@@ -4,20 +4,24 @@ import android.Manifest
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
+import android.util.Log
 import android.view.View
 import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.LocationServices
 import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
-    companion object{
+    companion object {
         const val ID_LOCATION_PERMISSION = 0
     }
 
@@ -32,56 +36,62 @@ class MainActivity : AppCompatActivity() {
     override fun onRequestPermissionsResult(
         requestCode: Int,
         permissions: Array<out String>,
-        grantResults: IntArray) {
+        grantResults: IntArray
+    ) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == ID_LOCATION_PERMISSION){
-            if (grantResults[0]  == PackageManager.PERMISSION_GRANTED ||
-                    grantResults[1] == PackageManager.PERMISSION_GRANTED){
+        if (requestCode == ID_LOCATION_PERMISSION) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED ||
+                grantResults[1] == PackageManager.PERMISSION_GRANTED
+            ) {
                 Toast.makeText(this, "Berhasil di izinkan", Toast.LENGTH_SHORT).show()
 
-                if (!isLocationEnabled()){
+                if (!isLocationEnabled()) {
                     Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
                     startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
                 }
-            }else{
+            } else {
                 Toast.makeText(this, "Gagal di izinkan", Toast.LENGTH_SHORT).show()
             }
         }
     }
 
     private fun checkPermissionLocation() {
-        if (checkPermission()){
-            if (!isLocationEnabled()){
+        if (checkPermission()) {
+            if (!isLocationEnabled()) {
                 Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
                 startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
             }
-        }else{
+        } else {
             requestPermission()
         }
 
     }
 
-    private fun checkPermission(): Boolean{
-        if (ActivityCompat.checkSelfPermission(this,
+    private fun checkPermission(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
                 Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this,
+            ) == PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                this,
                 Manifest.permission.ACCESS_FINE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED){
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
             return true
         }
         return false
     }
 
-    private fun isLocationEnabled(): Boolean{
+    private fun isLocationEnabled(): Boolean {
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)){
+            locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+        ) {
             return true
         }
         return false
     }
 
-    private fun requestPermission(){
+    private fun requestPermission() {
         ActivityCompat.requestPermissions(
             this,
             arrayOf(
@@ -94,7 +104,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun onClick() {
-        fabCheckIn.setOnClickListener{
+        fabCheckIn.setOnClickListener {
             loadScanLocation()
             Handler().postDelayed({
                 getLastLocation()
@@ -102,20 +112,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadScanLocation(){
+    private fun loadScanLocation() {
         rippleBackground.startRippleAnimation()
         tvScanning.visibility = View.VISIBLE
         tvCheckInSuccess.visibility = View.GONE
     }
 
-    private fun stopScanLocation(){
+    private fun stopScanLocation() {
         rippleBackground.stopRippleAnimation()
         tvScanning.visibility = View.GONE
     }
 
-    private fun getLastLocation(){
-        if (checkPermission()){
-            if (isLocationEnabled()){
+    private fun getLastLocation() {
+        if (checkPermission()) {
+            if (isLocationEnabled()) {
                 LocationServices.getFusedLocationProviderClient(this).lastLocation.addOnSuccessListener { location ->
                     val currentLat = location.latitude
                     val currentLong = location.longitude
@@ -123,14 +133,27 @@ class MainActivity : AppCompatActivity() {
                     tvCheckInSuccess.visibility = View.VISIBLE
                     tvCheckInSuccess.text = "lat: $currentLat. lon: $currentLong"
 
+                    Log.d("coba", "size: ${getAddresses().size}")
+
+                    for (address: Address in getAddresses()){
+                        Log.d("coba", "lat: ${address.latitude}, lon ${address.longitude}")
+                    }
+
                     stopScanLocation()
                 }
-            }else{
+            } else {
                 Toast.makeText(this, "Please turn on your location", Toast.LENGTH_SHORT).show()
                 startActivity(Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS))
             }
-        }else{
+        } else {
             requestPermission()
         }
     }
+
+    private fun getAddresses(): List<Address> {
+        val destinationPlace = "Kominfo Kab.OKU"
+        val geocode = Geocoder(this, Locale.getDefault())
+        return geocode.getFromLocationName(destinationPlace, 100)
+    }
 }
+
